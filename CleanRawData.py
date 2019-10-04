b48eac273e76527b8a9f6d13deb3c73dfd2928b8
Holsapple <dholsapp@nrel.gov>
2019-10-04 10:51:29 -0600

    Created UniqueIDs for raw data.  Stroed the raw data as a tuple being a pickle.

--- CleanRawData.py
+++ CleanRawData.py
@@ -7,6 +7,7 @@ import glob
 import pandas as pd  # Pandas used to process data
 import io
 import pvlib
+import pickle
 
 try:
     # python 2 compatibility
@@ -32,22 +33,13 @@ class CleanRawData:
             
         path = currentDirectory
         
+        #Delete the current pickle files from the current directory
+        CleanRawData.deleteFile( path )
         
         dataFrames = CleanRawData.filesToDataFrame( path ) 
         
         
-        #First delete the content of the folder you will be sending the files to.
-        # We do this as organization to make sure all the files are current
-        for root, dirs, files in os.walk(path + '\\Pandas_Pickle_DataFrames\\Pickle_RawData'):
-            for f in files:
-                os.unlink(os.path.join(root, f))
-            for d in dirs:
-                shutil.rmtree(os.path.join(root, d))
-        for root, dirs, files in os.walk(path + '\\Pandas_Pickle_DataFrames\\Pickle_FirstRows'):
-            for f in files:
-                os.unlink(os.path.join(root, f))
-            for d in dirs:
-                shutil.rmtree(os.path.join(root, d))
+
         
 
         #Pull out the file names from the file path(.csv files) and return a list of file names without .csv extension
@@ -84,126 +76,64 @@ class CleanRawData:
     
     def filesToDataFrame( path ):
         
+        
+        
+        
+        
+
+        
+        #############################################################
+        
+        
+        
+        
+        
         allCsvFiles = glob.glob(path + '\Python_RawData_Combined' + "/*.csv")
         allEpwFiles = glob.glob(path + '\Python_RawData_Combined' + "/*.epw")
-  
-        dataFrameCsvlist = [] # Create a list to hold all the csv dataframes
-        dataFrameEpwlist = [] # Create a list to hold all the epw dataframes
         
+        
+        #Generate a Unique ID key for all files
+        totalNum = len(allCsvFiles + allEpwFiles)
+        
+        uniqueIDlist = []
+        for i in range( 0, totalNum + 1 ):
+            uniqueID = str( 999999 - i )
+            uniqueIDlist.append( uniqueID )
+            
+
+        
+        tmy3List = []
         #Create a list of dataframes for csv files
-        for i in range( 0 , len( allCsvFiles ) ):  
-            # (access the file, skip the first 1 rows)
-            csv_df = pd.read_csv(allCsvFiles[i], skiprows= 1 ,  header=0)
-            #Helper method, deltes NA data and renames column headers
-            if len(csv_df.columns) == 71:
-                csv_df = csv_df.drop(['PresWth source','PresWth uncert (code)'], axis=1)
-            csv_df = CleanRawData.renameFrame(csv_df)
-            dataFrameCsvlist.append(csv_df) # Keep adding dataframes to the end of the list
-    
+        for i in range( 0 , len( allCsvFiles ) ): 
+            
+            
+            # Read the pickle file
+            tmy3Data = pvlib.iotools.read_tmy3(filename=allCsvFiles[i], coerce_year=None, recolumn=True)
+            tmy3List.append( tmy3Data )
+            print(i)
+        
+ #####################################           
     ############################################################################
     #Will have to sort the df by unique identifier at some point
     ######################################################    
     
+        epwList = []
         #Create a list of dataframes for csv files
-        for i in range( 0 , len( allEpwFiles ) ):  
-            # Use helper method to convert the EPW file to a dataframe
-            epw_df = CleanRawData.read_epw_df(allEpwFiles[i] , coerce_year=None)
-            #Put the datetime objects into their own column by reseting the index
-            epw_df.reset_index(inplace=True)
-            #Convert the pandas time series to MM/DD/YYYY format
-            epw_df['Date (MM/DD/YYYY)'] = epw_df['index'].map(lambda x: x.strftime('%m/%d/%Y'))
-            #Convert the pandas time series to "Hour(24hrs scale):Minute"   format
-            epw_df['Time (HH:MM)'] = epw_df['index'].map(lambda x: x.strftime('%H:%M'))
-            #Convert the atmospheric pressure form Pa to mbar
-            epw_df['atmospheric_pressure'] = epw_df['atmospheric_pressure'].apply(lambda x: x/100)
-            #Convert the visibility to km to m
-            epw_df['visibility'] = epw_df['visibility'].apply(lambda x: x*1000)        
-            # Drop columns that we do not need
-            epw_df = epw_df.drop(['index', 
-                                  'year',
-                                  'month',
-                                  'day',
-                                  'hour',
-                                  'minute',
-                                  'data_source_unct'], 
-                                    axis=1)
-            #Re-index the columns in a proper fashion
-            epw_df = epw_df.reindex(columns = ['Date (MM/DD/YYYY)', 
-                                               'Time (HH:MM)',
-                                               'etr',
-                                               'etrn',
-                                               'ghi_infrared',
-                                               'ghi',
-                                               'dni', 
-                                               'dhi',
-                                               'global_hor_illum',
-                                               'direct_normal_illum', 
-                                               'diffuse_horizontal_illum',
-                                               'zenith_luminance',
-                                               'total_sky_cover',
-                                               'opaque_sky_cover',
-                                               'temp_air',
-                                               'temp_dew',
-                                               'relative_humidity',
-                                               'atmospheric_pressure',
-                                               'wind_direction',
-                                               'wind_speed',
-                                               'visibility', 
-                                               'ceiling_height',
-                                               'precipitable_water',
-                                               'aerosol_optical_depth',
-                                               'albedo',
-                                               'liquid_precipitation_depth',
-                                               'liquid_precipitation_quantity',
-                                               'present_weather_observation',
-                                               'present_weather_codes',
-                                               'snow_depth',
-                                               'days_since_last_snowfall'])                                          
-          #Rename the columns of .epw_df to match the .csv_df  ])    
-    
+        for j in range( 0 , len( allEpwFiles ) ):  
             
-    #Perform all cleaning to match the format of the CSVs
-            # Start with the first df
-           
-            epw_df.columns = ['Date (MM/DD/YYYY)', 
-                          'Time (HH:MM)',
-                          'Hourly extraterrestrial radiation on a horizontal surface',
-                          'Hourly extraterrestrial radiation normal to the sun',
-                          'Horizontal infrared radiation',
-                          'Global horizontal irradiance',
-                          'Direct normal irradiance',
-                          'Diffuse horizontal irradiance',
-                          'Global horizontal illuminance',
-                          'Direct normal illuminance',
-                          'Diffuse horizontal illuminance',
-                          'Zenith luminance',
-                          'Total sky cover',
-                          'Opaque sky cover',
-                          'Dry-bulb temperature',
-                          'Dew-point temperature',
-                          'Relative humidity',
-                          'Station pressure',
-                          'Wind direction',
-                          'Wind speed',
-                          'Horizontal visibility',
-                          'Ceiling height',
-                          'Precipitable water',
-                          'Aerosol optical depth, broadband',
-                          'Albedo',
-                          'Liquid percipitation depth',
-                          'Liquid percipitation quantity',
-                          'Present Weather Observations',
-                          'Present Weather Codes',
-                          'Snow Depth',
-                          'Days Since Last Snowfall']        
-            
-            
-            dataFrameEpwlist.append(epw_df) # Keep adding dataframes to the end of the list
+            epwData = pvlib.iotools.read_epw(filename=allEpwFiles[j] , coerce_year=None)
+            epwList.append(epwData) 
+            print(j)
     
-        #Combine both dataframe lists together
-        dataFrameCsvlist.extend(dataFrameEpwlist)
+        #Combine both tuple lists together
+        tmy3List.extend(epwList)
     
-    
+        for k in range(0 , len( tmy3List ) ):
+            with open(path + '\\Pandas_Pickle_DataFrames\\Pickle_RawData\\' + uniqueIDlist[k] + '.pickle', 'wb') as filename:
+                pickle.dump(tmy3List[k], filename)
+      
+        
+        
         return dataFrameCsvlist    
 
     '''
@@ -710,3 +640,21 @@ class CleanRawData:
                     
                     
         return sampleList       
+    
+    
+    
+    
+    
+    def deleteFile( path ):
+            #First delete the content of the folder you will be sending the files to.
+        # We do this as organization to make sure all the files are current
+        for root, dirs, files in os.walk(path + '\\Pandas_Pickle_DataFrames\\Pickle_RawData'):
+            for f in files:
+                os.unlink(os.path.join(root, f))
+            for d in dirs:
+                shutil.rmtree(os.path.join(root, d))
+        for root, dirs, files in os.walk(path + '\\Pandas_Pickle_DataFrames\\Pickle_FirstRows'):
+            for f in files:
+                os.unlink(os.path.join(root, f))
+            for d in dirs:
+                shutil.rmtree(os.path.join(root, d))
